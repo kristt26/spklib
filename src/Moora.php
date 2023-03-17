a8b105d54040fc66412d117b99faad42a9e3a35c
Ajenkris Yanto Kungkung <kristt26@gmail.com>
2023-03-18 01:48:10 +0900

    ok

--- src/Moora.php
+++ src/Moora.php
@@ -25,8 +25,8 @@ class Moora
         $this->alternatif = $alternatif;
         $this->matriksKeputusan = $this->setMatriksKeputusan($this->alternatif);
         $this->matriksNormalisasi = $this->setMatriksNormalisasi($this->alternatif, $this->kriteria);
-        $this->preferensi = $this->bobotPreferensi($this->vector);
-        $this->ranking = $this->rank($this->preferensi);
+        $this->nilaiOptimasi = $this->setNilaiOptimasi();
+        $this->ranking = $this->rank($this->nilaiOptimasi);
     }
 
     private function setMatriksKeputusan(array $alternatifs): array
@@ -46,55 +46,46 @@ class Moora
     {
         $data = [];
         foreach ($alternatifs as $keyAlternatif => $alternatif) {
-            foreach ($kriterias as $keyKriteria => $kriteria) {
+            $nilaiAlter = [];
+            foreach ($alternatif['nilai'] as $keyNilai => $nilai) {
+                $item = $nilai['bobot']/sqrt($this->getSum($nilai['kode']));
+                $alternatif[$keyAlternatif]['nilai'][$keyNilai]['normalMatriks']= $item;
+                array_push($nilaiAlter, $item);
             }
+            array_push($data, $nilaiAlter);
         }
         return $data;
     }
 
-    private function getSum($kode): int
+    private function getSum(string $kode): int
     {
         $item = 0;
         foreach ($this->alternatif as $key1 => $alternatif) {
             foreach ($alternatif['nilai'] as $key2 => $value) {
-                if ($value['kode'] == $kode) $item += $value['bobot'];
+                if ($value['kode'] == $kode) $item += pow($value['bobot'],2);
             }
         }
         return $item;
     }
 
-    private function normalisasiAlternatif(array $alternatif, array $bobot): array
+    private function setNilaiOptimasi(): array
     {
-        foreach ($alternatif as $keyAlternatif => $valueAlternatif) {
-            $nilai = 0;
-            foreach ($valueAlternatif['nilai'] as $keyNilai => $valueNilai) {
-                foreach ($bobot as $keyBobot => $valueBobot) {
-                    if ($valueNilai['kode'] == $valueBobot['kode']) {
-                        $alternatif[$keyAlternatif]['nilai'][$keyNilai]['bobot'] = pow(floatval($valueNilai['bobot']), ($valueBobot['type'] == 'Benefits' ? floatval($valueBobot['bobot']) : (floatval($valueBobot['bobot']) * -1)));
-                        if ($keyNilai == 0) {
-                            $nilai = $alternatif[$keyAlternatif]['nilai'][$keyNilai]['bobot'];
-                        } else {
-                            $nilai *= $alternatif[$keyAlternatif]['nilai'][$keyNilai]['bobot'];
-                        }
-                    }
-                }
+        $data = [];
+        foreach ($this->alternatif as $keyAlternatif => $alternatif) {
+            $item = 0;
+            foreach ($alternatif['nilai'] as $key => $nilai) {
+                $item += ($nilai['bobot'] * $this->cekJenis($nilai['kode']));
             }
-            $alternatif[$keyAlternatif]['vector'] = $nilai;
+            array_push($data, $item);
         }
-        return $alternatif;
+        return $data;
     }
 
-    private function bobotPreferensi(array $vector): array
+    private function cekJenis(string $kode): int
     {
-        $sum = 0;
-        foreach ($vector as $key => $value) {
-            $sum += $value['vector'];
+        foreach ($this->kriteria as $keyKriteria => $kriteria) {
+            if($kriteria['kode']==$kode) return $kriteria['type']=='Benefits' ? $kriteria['bobot'] : -$kriteria['bobot'];
         }
-
-        foreach ($vector as $key => $value) {
-            $vector[$key]['preferensi'] = floatval($value['vector']) / floatval($sum);
-        }
-        return $vector;
     }
 
     private function rank(array $data): array
