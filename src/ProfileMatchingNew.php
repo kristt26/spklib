b8bcb3dcf506755b3f312ea02c36137ec640eaa9
Ajenkris Yanto Kungkung <kristt26@gmail.com>
2023-07-24 19:16:44 +0900

    ok

---
+++ src/ProfileMatchingNew.php
@@ -0,0 +1,178 @@
+<?php
+
+namespace ocs\spklib;
+
+// use stdClass;
+
+class ProfileMatchingNew
+{
+    protected $limit;
+    protected $rounding;
+    protected $sf;
+    protected $kriteria;
+    public $alternatif;
+    public $nilaiAkhir;
+    public $rank;
+
+    public function __construct($kriteria, $alternatif, $limit, $sf, $rounding)
+    {
+        $this->kriteria = $kriteria;
+        $this->alternatif = $alternatif;
+        $this->limit = $limit;
+        $this->rounding = $rounding;
+        $this->sf = $sf;
+        $this->setMapGap();
+        $this->nilaiAkhir = $this->setNilaiAkhir();
+        $this->rank = $this->setRank($this->nilaiAkhir);
+    }
+
+    private function getBobotGap(float $nilai): float
+    {
+
+        $data = [
+            [
+                'gap' => 0,
+                'nilai' => 5
+            ],
+            [
+                'gap' => 1,
+                'nilai' => 4.5
+            ],
+            [
+                'gap' => -1,
+                'nilai' => 4
+            ],
+            [
+                'gap' => 2,
+                'nilai' => 3.5
+            ],
+            [
+                'gap' => -2,
+                'nilai' => 3
+            ],
+            [
+                'gap' => 3,
+                'nilai' => 2.5
+            ],
+            [
+                'gap' => -3,
+                'nilai' => 2
+            ],
+            [
+                'gap' => 4,
+                'nilai' => 1.5
+            ],
+            [
+                'gap' => -4,
+                'nilai' => 1
+            ]
+        ];
+        $item = 0;
+        foreach ($data as $key => $value) {
+            if ($value['gap'] == $nilai) {
+                $item = $value['nilai'];
+            }
+        }
+        return $item;
+    }
+
+    private function setMapGap()
+    {
+        if ($this->sf) {
+            foreach ($this->alternatif as $keyAlt => $alternatif) {
+                $nilaiAkhir = 0;
+                foreach ($alternatif['nilai'] as $keyNil => $nilai) {
+                    foreach ($this->kriteria as $key => $kriteria) {
+
+                        if ($nilai['code'] == $kriteria['code']) {
+                            $nilaiSf = 0;
+                            $countSf = 0;
+                            $nilaiCf = 0;
+                            $countCf = 0;
+                            foreach ($nilai['sub'] as $keySub => $sub) {
+                                foreach ($kriteria['sub'] as $keySubKriteria => $subKriteria) {
+                                    if ($sub['code'] == $subKriteria['code']) {
+                                        $this->alternatif[$keyAlt]['nilai'][$keyNil]['sub'][$keySub]['gap'] = $sub['nilai'] - $subKriteria['profileKriteria'];
+                                        $mapGap = $this->getBobotGap($sub['nilai'] - $subKriteria['profileKriteria']);
+                                        $this->alternatif[$keyAlt]['nilai'][$keyNil]['sub'][$keySub]['mapGap'] = $mapGap;
+                                        $nilaiSf += $this->getBobotGap($sub['nilai'] - $kriteria['profileKriteria']) * $subKriteria['bobot'];
+                                        if ($subKriteria['status'] == 'CF') {
+                                            $countCf += 1;
+                                            $nilaiCf += $mapGap;
+                                        }else{
+                                            $countSf += 1;
+                                            $nilaiSf += $mapGap;
+                                        }
+                                    }
+                                }
+                            }
+                            $nak = ($nilaiCf/$countCf*0.6)+($nilaiSf/$countSf*0.4);
+                            $this->alternatif[$keyAlt]['nilai'][$keyNil]['nak'] = $nak;
+                            $nilaiAkhir += ($nak*$kriteria['bobot']);
+                        }
+                        $this->alternatif[$keyAlt]['nilai'][$keyNil]['nak'] = $nak;
+                    }
+                }
+                $this->alternatif[$keyAlt]['nilaiAkhir'] = $nilaiAkhir;
+            }
+        } else {
+            foreach ($this->alternatif as $keyAlt => $alternatif) {
+                foreach ($alternatif['nilai'] as $keyNil => $nilai) {
+                    foreach ($this->kriteria as $key => $kriteria) {
+                        if ($nilai['code'] == $kriteria['code']) {
+                            $this->alternatif[$keyAlt]['nilai'][$keyNil]['gap'] = $nilai['nilai'] - $kriteria['profileKriteria'];
+                            $this->alternatif[$keyAlt]['nilai'][$keyNil]['mapGap'] = $this->getBobotGap($nilai['nilai'] - $kriteria['profileKriteria']);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    private function setNilaiAkhir(): array
+    {
+        $data = [];
+        if ($this->sf) {
+            foreach ($this->alternatif as $keyAlt => $alternatif) {
+                $nilai1 = 0;
+                foreach ($alternatif['nilai'] as $keyNil => $nilai) {
+                    foreach ($this->kriteria as $key => $kriteria) {
+                        if ($nilai['code'] == $kriteria['code']) {
+                            $hitung = ($nilai['sf'] * $kriteria['bobot']);
+                            $nilai1 += $hitung;
+                        }
+                    }
+                }
+                array_push($data, ['nama' => $this->alternatif[$keyAlt]['nama'], 'nilaiAkhir' => $nilai1 / count($this->kriteria)]);
+            }
+        } else {
+            foreach ($this->alternatif as $keyAlt => $alternatif) {
+                $nilai = 0;
+                foreach ($alternatif['nilai'] as $keyNil => $nilai) {
+                    foreach ($this->kriteria as $key => $kriteria) {
+                        if ($nilai['code'] == $kriteria['code']) {
+                            $nilai += ($nilai['mapGap'] * $kriteria['bobot']);
+                        }
+                    }
+                }
+                $item = ['nama' => $alternatif['nama'], 'nilaiAkhir' => $nilai];
+                array_push($data, $item);
+            }
+        }
+        return $data;
+    }
+
+    private function setRank(array $data): array
+    {
+        usort($data, function ($a, $b) {
+            $retval = $b['nilaiAkhir'] <=> $a['nilaiAkhir'];
+            return $retval;
+        });
+
+        for ($i = 0; $i < count($data); $i++) {
+            $data[$i]['nilaiAkhir'] = round($data[$i]['nilaiAkhir'], $this->rounding);
+        }
+
+        return $this->limit > 0 ? array_slice($data, 0, $this->limit) : $data;
+    }
+}
--- src/test_pf.php
+++ src/test_pf.php
@@ -1,76 +1,165 @@
 <?php
 
-require "ProfileMatching.php";
+require "ProfileMatchingNew.php";
 
-use ocs\spklib\ProfileMatching as PM;
+use ocs\spklib\ProfileMatchingNew as PM;
 
 $kriteria = [
     [
         'nama' => 'Kriteria 1',
         'code' => 'C1',
-        'profileKriteria' => 5,
-        'bobot' => 0.4,
+        'bobot' => 0.3,
         'sub' => [
             [
                 'nama' => 'Sub 11',
                 'code' => 'C11',
-                'bobot' => 0.7
+                'profileKriteria' => 4,
+                'status'=>'SF'
             ],
             [
                 'nama' => 'Sub 12',
                 'code' => 'C12',
-                'bobot' => 0.3
+                'profileKriteria' => 2,
+                'status'=>'SF'
+            ],
+            [
+                'nama' => 'Sub 13',
+                'code' => 'C13',
+                'profileKriteria' => 3,
+                'status'=>'SF'
+            ],
+            [
+                'nama' => 'Sub 14',
+                'code' => 'C14',
+                'profileKriteria' => 3,
+                'status'=>'CF'
             ]
+
         ]
     ],
     [
         'nama' => 'Kriteria 2',
         'code' => 'C2',
-        'profileKriteria' => 4,
-        'bobot' => 0.3,
+        'bobot' => 0.25,
         'sub' => [
             [
                 'nama' => 'Sub 21',
                 'code' => 'C21',
-                'bobot' => 0.5
+                'profileKriteria' => 3,
+                'status'=>'SF'
             ],
             [
                 'nama' => 'Sub 22',
                 'code' => 'C22',
-                'bobot' => 0.4
+                'profileKriteria' => 2,
+                'status'=>'SF'
             ],
             [
                 'nama' => 'Sub 23',
                 'code' => 'C23',
-                'bobot' => 0.1
+                'profileKriteria' => 4,
+                'status'=>'CF'
+            ],
+            [
+                'nama' => 'Sub 23',
+                'code' => 'C24',
+                'profileKriteria' => 3,
+                'status'=>'SF'
             ],
         ]
     ],
     [
         'nama' => 'Kriteria 3',
         'code' => 'C3',
-        'profileKriteria' => 4,
-        'bobot' => 0.3,
+
+        'bobot' => 0.2,
         'sub' => [
             [
                 'nama' => 'Sub 31',
                 'code' => 'C31',
-                'bobot' => 0.25
+                'profileKriteria' => 3,
+                'status'=>'SF'
             ],
             [
                 'nama' => 'Sub 32',
                 'code' => 'C32',
-                'bobot' => 0.25
+                'profileKriteria' => 3,
+                'status'=>'CF'
             ],
             [
                 'nama' => 'Sub 33',
                 'code' => 'C33',
-                'bobot' => 0.25
+                'profileKriteria' => 2,
+                'status'=>'SF'
             ],
             [
                 'nama' => 'Sub 34',
                 'code' => 'C34',
-                'bobot' => 0.25
+                'profileKriteria' => 3,
+                'status'=>'SF'
+            ],
+        ]
+    ],
+    [
+        'nama' => 'Kriteria 4',
+        'code' => 'C4',
+
+        'bobot' => 0.15,
+        'sub' => [
+            [
+                'nama' => 'Sub 41',
+                'code' => 'C41',
+                'profileKriteria' => 2,
+                'status'=>'SF'
+            ],
+            [
+                'nama' => 'Sub 42',
+                'code' => 'C42',
+                'profileKriteria' => 2,
+                'status'=>'CF'
+            ],
+            [
+                'nama' => 'Sub 43',
+                'code' => 'C43',
+                'profileKriteria' => 4,
+                'status'=>'SF'
+            ],
+            [
+                'nama' => 'Sub 44',
+                'code' => 'C44',
+                'profileKriteria' => 3,
+                'status'=>'SF'
+            ],
+        ]
+    ],
+    [
+        'nama' => 'Kriteria 5',
+        'code' => 'C5',
+        'bobot' => 0.1,
+        'sub' => [
+            [
+                'nama' => 'Sub 51',
+                'code' => 'C51',
+                'profileKriteria' => 3,
+                'status'=>'SF'
+            ],
+            [
+                'nama' => 'Sub 52',
+                'code' => 'C52',
+                'profileKriteria' => 3,
+                'status'=>'SF'
+            ],
+            [
+                'nama' => 'Sub 53',
+                'code' => 'C53',
+                'profileKriteria' => 2,
+                'status'=>'SF'
+            ],
+            [
+                'nama' => 'Sub 54',
+                'code' => 'C54',
+                'profileKriteria' => 2,
+                'status'=>'CF'
             ],
         ]
     ],
@@ -83,12 +172,20 @@ $alternatif = [
                 "code" => "C1",
                 "sub" => [
                     [
-                        'code'=>'C11',
-                        'nilai'=> 5
+                        'code' => 'C11',
+                        'nilai' => 5
                     ],
                     [
-                        'code'=>'C12',
-                        'nilai'=> 4
+                        'code' => 'C12',
+                        'nilai' => 4
+                    ],
+                    [
+                        'code' => 'C13',
+                        'nilai' => 4
+                    ],
+                    [
+                        'code' => 'C14',
+                        'nilai' => 4
                     ],
                 ]
             ],
@@ -96,16 +193,20 @@ $alternatif = [
                 "code" => "C2",
                 "sub" => [
                     [
-                        'code'=>'C21',
-                        'nilai'=> 5
+                        'code' => 'C21',
+                        'nilai' => 5
                     ],
                     [
-                        'code'=>'C22',
-                        'nilai'=> 5
+                        'code' => 'C22',
+                        'nilai' => 5
                     ],
                     [
-                        'code'=>'C23',
-                        'nilai'=> 4
+                        'code' => 'C23',
+                        'nilai' => 4
+                    ],
+                    [
+                        'code' => 'C24',
+                        'nilai' => 2
                     ],
                 ]
             ],
@@ -113,23 +214,65 @@ $alternatif = [
                 "code" => "C3",
                 "sub" => [
                     [
-                        'code'=>'C31',
-                        'nilai'=> 5
+                        'code' => 'C31',
+                        'nilai' => 5
                     ],
                     [
-                        'code'=>'C32',
-                        'nilai'=> 5
+                        'code' => 'C32',
+                        'nilai' => 5
                     ],
                     [
-                        'code'=>'C33',
-                        'nilai'=> 4
+                        'code' => 'C33',
+                        'nilai' => 4
                     ],
                     [
-                        'code'=>'C34',
-                        'nilai'=> 4
+                        'code' => 'C34',
+                        'nilai' => 4
                     ],
                 ]
-            ]
+            ],
+            [
+                "code" => "C4",
+                "sub" => [
+                    [
+                        'code' => 'C41',
+                        'nilai' => 4
+                    ],
+                    [
+                        'code' => 'C42',
+                        'nilai' => 5
+                    ],
+                    [
+                        'code' => 'C43',
+                        'nilai' => 2
+                    ],
+                    [
+                        'code' => 'C44',
+                        'nilai' => 1
+                    ],
+                ]
+            ],
+            [
+                "code" => "C5",
+                "sub" => [
+                    [
+                        'code' => 'C51',
+                        'nilai' => 3
+                    ],
+                    [
+                        'code' => 'C52',
+                        'nilai' => 2
+                    ],
+                    [
+                        'code' => 'C53',
+                        'nilai' => 5
+                    ],
+                    [
+                        'code' => 'C54',
+                        'nilai' => 2
+                    ],
+                ]
+            ],
         ]
     ],
     [
@@ -139,12 +282,20 @@ $alternatif = [
                 "code" => "C1",
                 "sub" => [
                     [
-                        'code'=>'C11',
-                        'nilai'=> 4
+                        'code' => 'C11',
+                        'nilai' => 3
                     ],
                     [
-                        'code'=>'C12',
-                        'nilai'=> 4
+                        'code' => 'C12',
+                        'nilai' => 2
+                    ],
+                    [
+                        'code' => 'C13',
+                        'nilai' => 4
+                    ],
+                    [
+                        'code' => 'C14',
+                        'nilai' => 5
                     ],
                 ]
             ],
@@ -152,16 +303,20 @@ $alternatif = [
                 "code" => "C2",
                 "sub" => [
                     [
-                        'code'=>'C21',
-                        'nilai'=> 4
+                        'code' => 'C21',
+                        'nilai' => 3
                     ],
                     [
-                        'code'=>'C22',
-                        'nilai'=> 4
+                        'code' => 'C22',
+                        'nilai' => 3
                     ],
                     [
-                        'code'=>'C23',
-                        'nilai'=> 4
+                        'code' => 'C23',
+                        'nilai' => 1
+                    ],
+                    [
+                        'code' => 'C24',
+                        'nilai' => 5
                     ],
                 ]
             ],
@@ -169,82 +324,68 @@ $alternatif = [
                 "code" => "C3",
                 "sub" => [
                     [
-                        'code'=>'C31',
-                        'nilai'=> 4
+                        'code' => 'C31',
+                        'nilai' => 4
                     ],
                     [
-                        'code'=>'C32',
-                        'nilai'=> 4
+                        'code' => 'C32',
+                        'nilai' => 4
                     ],
                     [
-                        'code'=>'C33',
-                        'nilai'=> 4
+                        'code' => 'C33',
+                        'nilai' => 4
                     ],
                     [
-                        'code'=>'C34',
-                        'nilai'=> 4
+                        'code' => 'C34',
+                        'nilai' => 4
                     ],
                 ]
-            ]
+            ],
+            [
+                "code" => "C4",
+                "sub" => [
+                    [
+                        'code' => 'C41',
+                        'nilai' => 4
+                    ],
+                    [
+                        'code' => 'C42',
+                        'nilai' => 3
+                    ],
+                    [
+                        'code' => 'C43',
+                        'nilai' => 3
+                    ],
+                    [
+                        'code' => 'C44',
+                        'nilai' => 5
+                    ],
+                ]
+            ],
+            [
+                "code" => "C5",
+                "sub" => [
+                    [
+                        'code' => 'C51',
+                        'nilai' => 4
+                    ],
+                    [
+                        'code' => 'C52',
+                        'nilai' => 3
+                    ],
+                    [
+                        'code' => 'C53',
+                        'nilai' => 3
+                    ],
+                    [
+                        'code' => 'C54',
+                        'nilai' => 4
+                    ],
+                ]
+            ],
         ]
     ],
-    [
-        "nama" => "Nama3",
-        "nilai" => [
-            [
-                "code" => "C1",
-                "sub" => [
-                    [
-                        'code'=>'C11',
-                        'nilai'=> 5
-                    ],
-                    [
-                        'code'=>'C12',
-                        'nilai'=> 4
-                    ],
-                ]
-            ],
-            [
-                "code" => "C2",
-                "sub" => [
-                    [
-                        'code'=>'C21',
-                        'nilai'=> 4
-                    ],
-                    [
-                        'code'=>'C22',
-                        'nilai'=> 5
-                    ],
-                    [
-                        'code'=>'C23',
-                        'nilai'=> 5
-                    ],
-                ]
-            ],
-            [
-                "code" => "C3",
-                "sub" => [
-                    [
-                        'code'=>'C31',
-                        'nilai'=> 4
-                    ],
-                    [
-                        'code'=>'C32',
-                        'nilai'=> 5
-                    ],
-                    [
-                        'code'=>'C33',
-                        'nilai'=> 5
-                    ],
-                    [
-                        'code'=>'C34',
-                        'nilai'=> 4
-                    ],
-                ]
-            ]
-        ]
-    ]
 ];
 
-$a = new PM($kriteria, $alternatif, 0, true);
-echo json_encode($a->rank);
+$a = new PM($kriteria, $alternatif, 0, true, 0);
+echo json_encode($a);
